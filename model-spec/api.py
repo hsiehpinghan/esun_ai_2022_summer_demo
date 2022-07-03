365a429f0901c4f6cd8a6b57808febafdc9af7ce
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2022-07-03 23:08:52 +0800

    backup

--- model-spec/api.py
+++ model-spec/api.py
@@ -1,20 +1,20 @@
-import os
-import sys
-import json
 import torch
 import hashlib
 
-from absl import logging
-from flask import Blueprint, jsonify, request
+from flask import request
+from flask import jsonify
+from flask import Blueprint
 from datetime import datetime
-from inference.inference import get_answer, get_tokenizer, get_model
+from inference.inference import get_model
+from inference.inference import get_answer
+from inference.inference import get_tokenizer
 
 bp = Blueprint('api', __name__, url_prefix='')
-checkpoints = ['/home/hsiehpinghan/git/esun_ai_2022_summer/model/sub_model_0',
-               '/home/hsiehpinghan/git/esun_ai_2022_summer/model/sub_model_1',
-               '/home/hsiehpinghan/git/esun_ai_2022_summer/model/sub_model_2',
-               '/home/hsiehpinghan/git/esun_ai_2022_summer/model/sub_model_3',
-               '/home/hsiehpinghan/git/esun_ai_2022_summer/model/sub_model_4'] 
+checkpoints = ['/content/checkpoint/0',
+               '/content/checkpoint/1',
+               '/content/checkpoint/2',
+               '/content/checkpoint/3',
+               '/content/checkpoint/4'] 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tokenizer = get_tokenizer(checkpoints=checkpoints)
 model = get_model(checkpoints=checkpoints,
@@ -26,7 +26,7 @@ def inference():
     answer = get_answer(tokenizer=tokenizer,
                         model=model,
                         request=data,
-                        char_to_similarity_bert_ids_file_path='/home/hsiehpinghan/git/esun_ai_2022_summer_demo/model-spec/data/char_to_similarity_bert_ids.json',
+                        char_to_similarity_bert_ids_file_path='/content/data/char_to_similarity_bert_ids.json',
                         device=device)
     resp = {'esun_uuid': data['esun_uuid'],
             'server_uuid': generate_server_uuid(),
