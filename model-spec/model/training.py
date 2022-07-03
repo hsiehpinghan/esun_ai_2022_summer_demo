ec20ef9eeba68ddea0062714354efc0f2f895e39
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2022-07-03 09:14:47 +0800

    backup

--- model-spec/model/training.py
+++ model-spec/model/training.py
@@ -13,12 +13,12 @@ import torch.nn as nn
 import torchmetrics
 
 from tqdm import tqdm
-from datasets import load_metric
 from argparse import ArgumentParser
+from data import EsunDataModule
+from sub_model import EsunModel
 from collections import defaultdict
 from transformers import BertTokenizer
 from transformers import BertForMaskedLM
-from IPython.display import clear_output
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
@@ -30,10 +30,20 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
-MODEL_NAME = 'shibing624/macbert4csc-base-chinese'
-
 def parse_args():
     parser = ArgumentParser()
+    parser.add_argument('--model_name',
+                        type=str,
+                        help='huggingface model name')
+    parser.add_argument('--batch_size',
+                        type=int,
+                        help='batch size')
+    parser.add_argument('--split',
+                        type=int,
+                        help='what kfold split in used.')
+    parser.add_argument('--checkpoint_output_dir',
+                        type=str,
+                        help='checkpoint output dir')
     parser = Trainer.add_argparse_args(parent_parser=parser)
     args = parser.parse_args()
     return args
@@ -42,10 +52,10 @@ predict_output = None
 
 def main(args):
     global predict_output
-    tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
-    datamodule = EsunDataModule(data_dir=DATA_DIR,
-                                  batch_size=BATCH_SIZE,
-                                  tokenizer=tokenizer)
+    tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=args.model_name)
+    datamodule = EsunDataModule(batch_size=args.batch_size,
+                                tokenizer=tokenizer,
+                                split=args.split)
     datamodule.prepare_data()
     datamodule.setup(stage='fit')
     model = EsunModel(tokenizer=tokenizer)
@@ -53,10 +63,10 @@ def main(args):
                                          callbacks=[EarlyStopping(monitor='val_epoch_char_error_rate',
                                                                   patience=5,
                                                                   mode='min'),
-                                                    ModelCheckpoint(dirpath=OUTPUT_DIR,
+                                                    ModelCheckpoint(dirpath=os.path.join(args.checkpoint_output_dir, str(args.split)),
                                                                     filename='{epoch:02d}-{val_epoch_char_error_rate:.3f}',
                                                                     monitor='val_epoch_char_error_rate',
-                                                                    save_top_k=1,  # save the best model
+                                                                    save_top_k=1,
                                                                     mode='min',
                                                                     auto_insert_metric_name=True,
                                                                     save_weights_only=False,
@@ -67,3 +77,25 @@ def main(args):
                 datamodule=datamodule)
     trainer.validate(model=model,
                      datamodule=datamodule)
+
+if __name__ == '__main__':
+    sys.argv = [sys.argv[0]]
+    sys.argv += ['--accelerator', 'cpu']
+    sys.argv += ['--accumulate_grad_batches', '1']
+    #sys.argv += ['--auto_lr_find']  # not working !!!
+    #sys.argv += ['--auto_scale_batch_size', 'binsearch']
+    sys.argv += ['--benchmark']  #  if deterministic is set to True, this will default to False.
+    sys.argv += ['--device', '1']
+    #sys.argv += ['--fast_dev_run', '1']  # for test purpose.
+    #sys.argv += ['--deterministic']
+    sys.argv += ['--log_every_n_steps', '1']
+    sys.argv += ['--max_epochs', '100']
+    #sys.argv += ['--num_sanity_val_steps', '-1']
+    #sys.argv += ['--max_time', '00:00:10:00']
+    #sys.argv += ['--precision', '16']
+    #sys.argv += ['--max_steps', '1000']
+    sys.argv += ['--model_name', 'shibing624/macbert4csc-base-chinese']
+    sys.argv += ['--batch_size', '64']
+    sys.argv += ['--split', '0']
+    sys.argv += ['--checkpoint_output_dir', 'checkpoint']
+    main(args=parse_args())
